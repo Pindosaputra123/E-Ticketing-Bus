a07fee240d8cc648ff26426cd01564569d9184d9
Pindosaputra123 <pindoajja864@gmail.com>
2025-02-03 10:11:40 +0700

    Implementasi Login dengan Prepared Statement

---
+++ login.inc.php
@@ -0,0 +1,33 @@
+<?php
+    require("db.php");
+
+    session_start();
+
+    $inputEmail = $_POST["inputEmail"];
+    $inputPassword = ($_POST["inputPassword"]);
+
+    $stmt = $con->prepare("SELECT AccountNo,Email,Password FROM account WHERE Email = ?");
+    $stmt -> bind_param("s", $inputEmail);
+    $stmt -> execute();
+    $stmt -> store_result();
+
+    if($stmt -> num_rows > 0)
+    {
+        $stmt -> bind_result($accountNo,$accountEmail,$accountPassword);
+        $stmt -> fetch();
+        
+        if($inputPassword == $accountPassword)
+        {
+            $_SESSION["id"] = $accountNo;
+            header("location: index.php");
+        }
+        else
+        {
+            header("location: login.php?error=wrong_password");
+        }
+    }
+    else
+    {
+        header("location: login.php?error=no_account");
+    }
+?>
