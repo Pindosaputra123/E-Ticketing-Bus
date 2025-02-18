e5cb87d77b463bab445654e44d9c1ff70cc2b24f
Pindosaputra123 <pindoajja864@gmail.com>
2025-02-18 20:46:31 +0700

    Penyesuaian Nama Database

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $databasename = "s900_database";
+    $databasename = "bus";
 
     $con = mysqli_connect($servername,$username,$password,$databasename);
 
